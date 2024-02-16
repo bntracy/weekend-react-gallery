e33d1c9a6304c6d097ffe1ba6827464ea1bb6506
Beth Tracy <bntracy@gmail.com>
2024-02-16 16:00:39 -0600

    images added and database.sql updated

--- database.sql
+++ database.sql
@@ -9,5 +9,10 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
-('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
+('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
+('images/family.jpg', 'Family', 'Photo on vacation with my parents and brother.'),
+('images/friends1.jpg', 'Friends', 'My friends at Turducken. We are all very full.'),
+('images/friends2.jpg', 'Friends', 'My friends, ready for an adventure.'),
+('images/Maxie1.jpg', 'Maxie', 'Pretty bird!'),
+('images/Maxie2.jpg', 'Maxie', 'Maxie getting scratchies.');
   
