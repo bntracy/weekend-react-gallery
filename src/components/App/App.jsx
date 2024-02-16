8acec6016bab023d8a5bcd0a488147005c4d893f
Beth Tracy <bntracy@gmail.com>
2024-02-16 16:14:41 -0600

    components added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,14 @@
+import GalleryList from "../GalleryList/GalleryList";
 
 function App() {
     return (
-      <div>
+      <div data-testid="app">
         <header>
           <h1>React Gallery</h1>
         </header>
-
-        <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList />
+        {/* <p>The gallery goes here!</p>
+        <img src="images/goat_small.jpg"/> */}
       </div>
     );
 }
