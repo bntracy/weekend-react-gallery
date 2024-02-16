c75e71882c6ac7da0eba192de3e82099baa8bc04
Beth Tracy <bntracy@gmail.com>
2024-02-16 16:26:14 -0600

    GET works client-side. Still doesn't display anything.

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,5 +1,25 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function GalleryList() {
-    return (<></>);
+    const [galleryArray, setGalleryArray] = useState([]);
+
+    const fetchGallery = () => {
+        axios({
+            method: 'GET',
+            url: 'api/gallery/'
+        }).then(response => {
+            setGalleryArray(response.data);
+        }).catch(error => {
+            console.log('Error getting gallery', error);
+        });
+    }
+
+    useEffect(fetchGallery, []);
+
+    return (
+        <div data-testid="galleryList"></div>
+    );
 }
 
 export default GalleryList;
