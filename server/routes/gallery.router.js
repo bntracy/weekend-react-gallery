7b3531561a728754125727e1ae902d1a7bb7cc51
Beth Tracy <bntracy@gmail.com>
2024-02-16 16:08:53 -0600

    GET route added in server

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
@@ -8,7 +9,13 @@ router.put('/like/:id', (req, res) => {
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlText = 'SELECT * FROM "gallery"';
+  pool.query(sqlText).then(dbResult => {
+    res.send(dbResult.rows);
+  }).catch(dbError => {
+    console.log('Error getting data from database', dbError);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
