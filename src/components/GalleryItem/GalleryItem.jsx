6a8f68bd0fd1b0fd359b5f610095afd4b19f182d
Beth Tracy <bntracy@gmail.com>
2024-02-17 10:46:13 -0600

    description text now centered vertically & horizontally

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -23,9 +23,11 @@ function GalleryItem( {item, fetchGallery} ) {
 
     return (
         <div data-testid="galleryItem" className="gallery-item">
-            { showPic 
-                ? <img src={item.url} alt={item.description} data-testid="toggle" onClick={() => togglePic()} />
-                : <p data-testid="toggle" onClick={togglePic} className="description">{item.description}</p> }
+            <div className="item-content">
+                { showPic 
+                    ? <img src={item.url} alt={item.description} data-testid="toggle" onClick={() => togglePic()} />
+                    : <p data-testid="toggle" onClick={togglePic} className="description">{item.description}</p> }
+            </div>
             <p>{item.title}</p>
             <p>Likes: {item.likes}</p>
             <button data-testid="like" onClick={likeItem}>Like</button>
