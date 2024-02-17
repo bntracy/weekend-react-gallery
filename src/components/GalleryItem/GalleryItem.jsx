fb2c51e4785ae783dcc6fceea0fdb166f24c9b64
Beth Tracy <bntracy@gmail.com>
2024-02-16 18:49:03 -0600

    dimensions remain roughly consistent

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import './GalleryItem.css'
+
 function GalleryItem( {item} ) {
     const [showPic, setShowPic] = useState(true);
 
@@ -8,10 +10,10 @@ function GalleryItem( {item} ) {
     }
 
     return (
-        <div data-testid="galleryItem">
+        <div data-testid="galleryItem" className="gallery-item">
             { showPic 
                 ? <img src={item.url} alt={item.description} data-testid="toggle" onClick={() => togglePic()} />
-                : <p data-testid="toggle" onClick={() => togglePic()}>{item.description}</p> }
+                : <p data-testid="toggle" onClick={() => togglePic()} className="description">{item.description}</p> }
             <p>{item.title}</p>
         </div>
     );
